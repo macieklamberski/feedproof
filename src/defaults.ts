d79b2b98112fdaf683db20618c6e9dc0f60f7e5c
Maciej Lamberski <maciej@lamberski.com>
2026-04-11 17:55:00 +0200

    feat: Compose default transform pipeline and embed resolver

---
+++ src/defaults.ts
@@ -0,0 +1,56 @@
+import { soundcloudEmbedDomains } from './platforms/soundcloud.js'
+import { spotifyEmbedDomains } from './platforms/spotify.js'
+import { vimeoEmbedDomains } from './platforms/vimeo.js'
+import { youtubeEmbedDomains, youtubeResolveEmbed } from './platforms/youtube.js'
+import { decodeDoubleEncodedTags } from './transforms/decodeDoubleEncodedTags.js'
+import { fixLazyImages } from './transforms/fixLazyImages.js'
+import { highlightCode } from './transforms/highlightCode.js'
+import { injectEnclosureEmbedPlaceholders } from './transforms/injectEnclosureEmbedPlaceholders.js'
+import { linkifyUrls } from './transforms/linkifyUrls.js'
+import { mergeConsecutiveOneLinerPres } from './transforms/mergeConsecutiveOneLinerPres.js'
+import { paragraphizePlainText } from './transforms/paragraphizePlainText.js'
+import { removeTrackingPixels } from './transforms/removeTrackingPixels.js'
+import { replaceMediaWithEmbedPlaceholders } from './transforms/replaceMediaWithEmbedPlaceholders.js'
+import { replacePreLineBreaks } from './transforms/replacePreLineBreaks.js'
+import { resolveRelativeUrls } from './transforms/resolveRelativeUrls.js'
+import { stripInterBlockBreaks } from './transforms/stripInterBlockBreaks.js'
+import { stripTrackingParams } from './transforms/stripTrackingParams.js'
+import { trimPreWhitespace } from './transforms/trimPreWhitespace.js'
+import { unwrapRedirectUrls } from './transforms/unwrapRedirectUrls.js'
+import { unwrapWrappers } from './transforms/unwrapWrappers.js'
+import type { DomTransform, EmbedResolverResult, StringTransform } from './types.js'
+
+export const defaultStringTransforms: Array<StringTransform> = [
+  decodeDoubleEncodedTags,
+  unwrapWrappers,
+  paragraphizePlainText,
+]
+
+export const defaultDomTransforms: Array<DomTransform> = [
+  fixLazyImages,
+  resolveRelativeUrls,
+  unwrapRedirectUrls,
+  stripTrackingParams,
+  removeTrackingPixels,
+  stripInterBlockBreaks,
+  highlightCode,
+  mergeConsecutiveOneLinerPres,
+  replacePreLineBreaks,
+  trimPreWhitespace,
+  linkifyUrls,
+  replaceMediaWithEmbedPlaceholders,
+  injectEnclosureEmbedPlaceholders,
+]
+
+export const defaultEmbedDomains: Array<string> = [
+  ...youtubeEmbedDomains,
+  ...vimeoEmbedDomains,
+  ...spotifyEmbedDomains,
+  ...soundcloudEmbedDomains,
+]
+
+export const defaultResolveEmbed = (url: string): EmbedResolverResult | undefined => {
+  return youtubeResolveEmbed(url)
+}
+
+export const defaultLazySrcAttributes = ['data-src', 'data-original', 'data-lazy-src', 'data-url']
