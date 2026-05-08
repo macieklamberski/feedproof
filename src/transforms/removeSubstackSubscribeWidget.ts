59224c6c6de717a57b3bdf6d776c614aedabeaff
Maciej Lamberski <maciej@lamberski.com>
2026-05-08 10:12:20 +0200

    refactor: Extract widgets query to const

--- src/transforms/removeSubstackSubscribeWidget.ts
+++ src/transforms/removeSubstackSubscribeWidget.ts
@@ -2,7 +2,9 @@ import type { DomTransform } from '../types.js'
 
 export const removeSubstackSubscribeWidget: DomTransform = () => {
   return (document) => {
-    for (const widget of document.querySelectorAll('[data-component-name="SubscribeWidget"]')) {
+    const widgets = document.querySelectorAll('[data-component-name="SubscribeWidget"]')
+
+    for (const widget of widgets) {
       widget.remove()
     }
   }
