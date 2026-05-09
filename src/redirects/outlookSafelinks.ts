8c009b2073d6226976e1580c5973267e465ea099
Maciej Lamberski <maciej@lamberski.com>
2026-05-09 09:02:17 +0200

    refactor: Use feedscout host helpers in Outlook SafeLinks extractor

--- src/redirects/outlookSafelinks.ts
+++ src/redirects/outlookSafelinks.ts
@@ -1,8 +1,9 @@
+import { isSubdomainOf } from 'feedscout/utils'
 import type { RedirectExtractor } from '../types.js'
 
 // Outlook SafeLinks (<tenant>.safelinks.protection.outlook.com/?url=<target>).
 export const extractOutlookSafelinks: RedirectExtractor = (url) => {
-  if (url.hostname.endsWith('.safelinks.protection.outlook.com')) {
+  if (isSubdomainOf(url.href, 'safelinks.protection.outlook.com')) {
     return url.searchParams.get('url') ?? null
   }
 
