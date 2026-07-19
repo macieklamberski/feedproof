edb1f104fcfbcc680577ea21f7a91524fd73cbda
Maciej Lamberski <maciej@lamberski.com>
2026-07-19 21:28:04 +0200

    feat: Convert dev.to embed cards into bookmark placeholders

--- src/defaults.ts
+++ src/defaults.ts
@@ -1,5 +1,6 @@
 import { resolveUrl } from 'feedcanon'
 import { cocoonBookmarkResolver } from './bookmarks/cocoon.js'
+import { devtoBookmarkResolver } from './bookmarks/devto.js'
 import { discourseBookmarkResolver } from './bookmarks/discourse.js'
 import { ghostBookmarkResolver } from './bookmarks/ghost.js'
 import {
@@ -235,6 +236,7 @@ export const defaultBookmarkResolvers: Array<BookmarkResolver> = [
   discourseBookmarkResolver,
   swellBookmarkResolver,
   xenforoBookmarkResolver,
+  devtoBookmarkResolver,
 ]
 
 export const defaultResolveUrlFn: ResolveUrlFn = (url, baseUrl) => resolveUrl(url, baseUrl)
