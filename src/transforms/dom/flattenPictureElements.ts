4c7eeebbbd7470909890f53d79fcdfeecae9e4c9
Maciej Lamberski <maciej@lamberski.com>
2026-08-20 15:41:24 +0200

    refactor: Mint every image through one builder

--- src/transforms/dom/flattenPictureElements.ts
+++ src/transforms/dom/flattenPictureElements.ts
@@ -1,5 +1,6 @@
 import type { DomTransform } from '../../types.js'
 import { parseSrcset } from '../../utils/images.js'
+import { createImage } from '../../utils/widgets.js'
 
 // Prefer AVIF, then WebP. Other source types are not worth promoting over the
 // <img> fallback, which is already a widely-supported format.
@@ -117,10 +118,7 @@ export const flattenPictureElements: DomTransform = () => {
         continue
       }
 
-      const image = document.createElement('img')
-      image.setAttribute('src', url)
-      image.setAttribute('srcset', srcset)
-      picture.replaceWith(image)
+      picture.replaceWith(createImage(document, { src: url, srcset }))
     }
   }
 }
