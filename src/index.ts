c9888f50896d548d2d5d3c52f54bb9d2f8cc9b4e
Maciej Lamberski <maciej@lamberski.com>
2026-09-04 20:00:09 +0200

    feat: Strip a paragraph that only links to the item's own page

--- src/index.ts
+++ src/index.ts
@@ -144,6 +144,7 @@ export { stripInterBlockBreaks } from './transforms/dom/stripInterBlockBreaks.js
 export { stripLeadingIndentation } from './transforms/dom/stripLeadingIndentation.js'
 export { stripMarkdownEscapeBackslashes } from './transforms/dom/stripMarkdownEscapeBackslashes.js'
 export { stripNonContentElements } from './transforms/dom/stripNonContentElements.js'
+export { stripSelfLinkParagraphs } from './transforms/dom/stripSelfLinkParagraphs.js'
 export { stripWordBreaks } from './transforms/dom/stripWordBreaks.js'
 export { surfaceNoscriptEmbeds } from './transforms/dom/surfaceNoscriptEmbeds.js'
 export { surfaceParkedMarkup } from './transforms/dom/surfaceParkedMarkup.js'
